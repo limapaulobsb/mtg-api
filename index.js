96778559ff448fcc2225d86c466c700efaf17b18
Paulo Henrique Lima <limapaulobsb@gmail.com>
2022-02-03 19:12:48 -0300

    Get cards by Id and other test endpoints

--- index.js
+++ index.js
@@ -8,9 +8,79 @@ const PORT = 3000;
 
 app.use(cors());
 
-app.get('/', async (_req, res) => {
+app.get('/api/v1/oracle', async (req, res) => {
+  const { n } = req.query;
+  const re = new RegExp(`^${n}$`, 'i');
   const conn = await connect();
-  const query = await conn.collection('allCards').find().limit(50).toArray();
+
+  const { oracle_id: id } = await conn.collection('all').findOne({
+    $or: [{ name: { $regex: re } }, { printed_name: { $regex: re } }],
+  });
+
+  return res.status(200).json({ id });
+});
+
+app.get('/api/v1/oracle/:id', async (req, res) => {
+  const { id } = req.params;
+  const conn = await connect();
+  const query = await conn.collection('oracle').find({ oracle_id: id }).toArray();
+
+  return res.status(200).json(query);
+});
+
+app.get('/api/v1/default/:id', async (req, res) => {
+  const { id } = req.params;
+  const conn = await connect();
+
+  const query = await conn
+    .collection('default')
+    .aggregate([
+      {
+        $match: {
+          $or: [{ id }, { oracle_id: id }],
+          digital: false,
+        },
+      },
+      {
+        $sort: {
+          released_at: -1,
+        },
+      },
+    ])
+    .toArray();
+
+  return res.status(200).json(query);
+});
+
+app.get('/api/v1/allcards/:id', async (req, res) => {
+  const { id } = req.params;
+  const conn = await connect();
+
+  const query = await conn
+    .collection('all')
+    .aggregate([
+      {
+        $match: {
+          $or: [{ id }, { oracle_id: id }],
+          digital: false,
+        },
+      },
+      {
+        $sort: {
+          released_at: -1,
+        },
+      },
+    ])
+    .toArray();
+
+  return res.status(200).json(query);
+});
+
+app.get('/api/v1/rulings/:id', async (req, res) => {
+  const { id } = req.params;
+  const conn = await connect();
+  const query = await conn.collection('rulings').findOne({ oracle_id: id });
+
   return res.status(200).json(query);
 });
 
